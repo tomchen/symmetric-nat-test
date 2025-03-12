9d4b36413fc475f306dab5401ac46a86cb9232ec
Tom Chen <tomchen.org@gmail.com>
2025-03-12 12:53:50 +0100

    ice server list; readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Symmetric NAT Test
 
 Go to [**the "Symmetric NAT Test" page (https://tomchen.github.io/symmetric-nat-test/)**](https://tomchen.github.io/symmetric-nat-test/) and see if you are behind a Symmetric NAT or Normal NAT (Full-cone NAT or other non-symmetric)
+
+My other WebRTC online tools:
+
+- [STUN Server Test](https://tomchen.github.io/stun-server-test/)
+- [WebRTC P2P Chat Test](https://tomchen.github.io/webrtc-p2p-chat/)
--- src/isNormalNat.ts
+++ src/isNormalNat.ts
@@ -54,8 +54,10 @@ export const isNormalNat = () =>
     const pc = new RTCPeerConnection({
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
         { urls: "stun:stun2.l.google.com:19302" },
-        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
       ],
     });
     pc.createDataChannel("foo");
